9a73f3d55c801c5d895dc187f84da7441566e1c5
Yan Wye Huong <wyehuong88@gmail.com>
2017-09-01 17:34:52 +0800

    added more questions

--- botui-app.js
+++ botui-app.js
@@ -1,10 +1,19 @@
 var botui = new BotUI('botui-app'); // give it the id of container
 
+function Node(key) {
+  this.children = []
+  this.parent = null
+
+  this.question = null
+  this.answers = []
+  this.responses = []
+}
+
 botui.message.bot({ // show first message
   delay: 200,
   content: 'hello there!'
 }).then(function () {
-  return botui.message.bot({ // second one
+  return botui.message.add({ // second one
     delay: 1000, // wait 1 sec.
     content: 'how are you?'
   });
@@ -41,6 +50,44 @@ botui.message.bot({ // show first message
 }).then(function (res) {
   return botui.message.add({
     delay: 1000, // wait 1 sec.
-    content: 'Your name is ' + res.value
+    content: 'nice to meet you, ' + res.value
+  });
+}).then(function() {
+  return botui.message.add({ // show a message
+    delay: 1000, // wait 1 sec.
+    content: 'what would you like to know about me?'
+  });
+}).then(function () {
+  return botui.action.button({ // let user do something
+    delay: 1000,
+    action: [
+      {
+        text: 'how old are you?',
+        value: 'how_old_are_you'
+      },
+      {
+        text: 'what are you working on currently?',
+        value: 'what_are_you_working_on_currently'
+      }
+    ]
+  });
+}).then(function (res) {
+  let response = ""
+
+  switch (res.value) {
+    case 'how_old_are_you':
+      response = "that's a secret..."
+      break;
+    case 'what_are_you_working_on_currently':
+      response = "i've just completed my [udacity nanodegree](https://www.udacity.com/course/deep-learning-nanodegree-foundation--nd101) and right now i'm learning golang!"
+      break;
+    default:
+      response = "hmmm... what?"
+      break;
+  }
+
+  return botui.message.add({ // show a message
+    delay: 1000, // wait 1 sec.
+    content: response
   });
 });
